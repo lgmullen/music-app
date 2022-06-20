a4519ccbe8bf9a1df0fc2947c4eed8a878359e25
Lawrence Mullen <lgmullen@umich.edu>
2022-06-20 15:19:02 -0400

    added tailwind css :0

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>sup nerds</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-96">
+	<h1 class="text-3xl text-center">sup nerds!</h1>
+	<p class="text-3xl text-center">
+		Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
