8fb62a11e530df18587d79c184b878799ce8c84e
Lawrence Mullen <lgmullen@umich.edu>
2022-06-21 11:32:51 -0400

    nuked everything

--- src/routes/index.svelte
+++
@@ -1,6 +0,0 @@
-<div class="h-96">
-	<h1 class="text-3xl text-center">sup nerds!</h1>
-	<p class="text-3xl text-center">
-		Visit <a class="underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-	</p>
-</div>
